2bc122ec2dda8a97e6299c435eb6eef6bfc97912
Sean Murphy <sean329@gmail.com>
2018-01-01 18:37:38 -0600

    refactored project so that imported sw is not pushed to git

--- js/photoGallery.js
+++ js/photoGallery.js
@@ -1,1 +1,24 @@
-$('.search-game').search();
+function filterDataTitleByTagAttribute(id, tag, attr) {
+  var input, filter, collection, i, element;
+  input = document.getElementById(id);
+  filter = input.value.toUpperCase();
+  collection = document.getElementsByTagName(tag);
+  for (i = 0; i < collection.length; i++) {
+    element = collection[i];
+    if (element.getAttribute(attr) != null &&
+      element.getAttribute(attr).toUpperCase().indexOf(filter) > -1) {
+      collection[i].style.display = "";
+    } else {
+      collection[i].style.display = "none";
+    }
+  }
+}
+
+// Run javascript after DOM is initialized
+$(document).ready(function () {
+  $('#search-photo').hideseek();
+
+  $('#search-photo').on("_after", function () {
+    filterDataTitleByTagAttribute("search-photo", "a", "data-title");
+  });
+});
